4d693a9caeeb0ea66daaf74f66a72ff049d9f856
MatheusGrundler <matheusgrundler@gmail.com>
2018-06-14 23:31:15 -0300

    MAIN finalizado
    término do main!

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$('.saiba').hover(
+  function(){ $(this).addClass('animated infinite pulse') },
+  function(){ $(this).removeClass('animated infinite pulse') }
+);
+$('.fa-arrow-alt-circle-down').hover(
+  function(){ $(this).addClass('animated infinite shake') },
+  function(){ $(this).removeClass('animated infinite shake') }
+);
