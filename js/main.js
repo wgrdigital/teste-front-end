4c07c58e961ec4bd52465349c40263fd3ae6561b
MatheusGrundler <matheusgrundler@gmail.com>
2018-06-16 12:17:20 -0300

    funcionalidades acabei +/-
    ta com alguns bugs,como, scroll e alinhamento

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $('.fa-arrow-alt-circle-down').hover(
   function(){ $(this).addClass('animated infinite shake') },
   function(){ $(this).removeClass('animated infinite shake') }
 );
+
+$('.opacity').hover(
+  function(){ $(this).addClass('opacity-1') },
+  function(){ $(this).removeClass('opacity-1') }
+);
+$('.item-1').hover(
+  function(){ $('.item-1 .some').addClass('opacity') },
+  function(){ $('.item-1 .some').removeClass('opacity') }
+);
+$('.item-2').hover(
+  function(){ $('.item-2 .some').addClass('opacity') },
+  function(){ $('.item-2 .some').removeClass('opacity') }
+);
+$('.item-3').hover(
+  function(){ $('.item-3 .some').addClass('opacity') },
+  function(){ $('.item-3 .some').removeClass('opacity') }
+);
+$('.item-4').hover(
+  function(){ $('.item-4 .some').addClass('opacity') },
+  function(){ $('.item-4 .some').removeClass('opacity') }
+);
